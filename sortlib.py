62b154eae5e8436d60f016ccc6217dcc5a166ba5
Sayan-Paul <sayan_666@yahoo.com>
2013-05-28 21:32:48 +0530

    bubble added ,selection corrected

--- sortlib.py
+++ sortlib.py
@@ -13,16 +13,29 @@ def insertion(ar):
 def selection(ar):
     for i in range(len(ar)):
         pos=i
-        sorte=True
+        sorted=True
         for j in range(i,len(ar)):
             if(ar[pos]>ar[j]):
                 pos=j
-                sorte=False
+                sorted=False
         ar[i],ar[pos]=ar[pos],ar[i]
-        if(sorte):
-            print i
+        if(sorted):
             break
         print ar
+
+def bubble(ar):
+    for i in range(len(ar)):
+        sorted=True
+        for j in range(len(ar)-i-1):
+            if(ar[j]>ar[j+1]):
+                ar[j],ar[j+1]=ar[j+1],ar[j]
+                sorted=False
+        if sorted:
+            break
+        print ar
+
+def quicksort(ar):
+    return
 
 if __name__=='__main__':
     unsorted=[int(x) for x in raw_input().split(" ")]
@@ -30,4 +43,6 @@ if __name__=='__main__':
     insertion(unsorted[:])
     print "Selection : "
     selection(unsorted[:])
+    print "Bubble : "
+    bubble(unsorted[:])
     
